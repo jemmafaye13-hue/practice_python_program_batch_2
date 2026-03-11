052fefde3223938a4721d2a20659ebd0f301c1e2
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-12 01:06:11 +0800

    add program to check if two numbers are not equal

---
+++ check_not_equal_numbers.py
@@ -0,0 +1,6 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if num1 != num2:
+    print(f"Not Equal")
