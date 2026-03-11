2318fb8b5113174fb67b17648c6b94aabb9a65f2
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-12 00:55:48 +0800

    add program to find the smaller of two numbers

---
+++ print_smaller_number.py
@@ -0,0 +1,5 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"The smaller number is: {min(num1, num2)}")
