90cac110c9c93f76dd46e8e499d6e3564103cfd4
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-12 01:31:55 +0800

    add program to calculate the remainder

---
+++ remainder_of_two_numbers.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"Remainder: {num1 % num2}")
+
