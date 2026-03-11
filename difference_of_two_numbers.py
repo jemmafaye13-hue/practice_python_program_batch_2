004d7a56a5ff048900e9602f13c185f01395c4f4
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-12 01:12:45 +0800

    add program to calculate the difference of two numbers

---
+++ difference_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter first number: "))
+print(f"Difference: {num1 - num2}")
